15306072a487bd46721bc23f0a8cff8d14a68ef9
lTworek1 <ltworek@icloud.com>
2024-09-22 19:19:38 +0200

    conteiners managment

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ from player import *
 def main():
 	pygame.init()
 	PyGame = pygame.get_init()
-	print(f"pygem initialization: {PyGame}")
-	
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-	
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	PyClock = pygame.time.Clock()
 	dt = 0
 	
 	player1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+	updatable.add(player1)
+	drawable.add(player1)
 
 	while True:
 		for event in pygame.event.get():
@@ -26,8 +26,12 @@ def main():
 		
 		FillColor = pygame.Color(0,0,0)
 		screen.fill(FillColor)
-		player1.update(dt)
-		player1.draw(screen)
+		for thing in updatable:
+			thing.update(dt)
+		
+		for thing in drawable:
+			thing.draw(screen)
+		
 		pygame.display.flip()
 					
 		mt = PyClock.tick(60)
